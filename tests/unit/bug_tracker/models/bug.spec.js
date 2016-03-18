e0afbccde4e94b04b4fdb4b4db6e5a2346a7fb9b
Joel Kraft <joelkraft3@yahoo.com>
2016-03-18 16:11:17 -0700

    test coverage for bug model

--- tests/unit/bug_tracker/models/bug.spec.js
+++ tests/unit/bug_tracker/models/bug.spec.js
@@ -14,6 +14,34 @@ define([
     });
 
     // @TODO write tests to match the implementation :)
+    it('should have defaults correctly set', function() {
+      expect(model.get('summary')).toEqual('');
+      expect(model.get('description')).toEqual('');
+      expect(model.get('editing')).toBe(true);
+    });
 
+    it('should validate summary to not be empty upon save', function() {
+      var msg = 'Summary needs to have content.';
+
+      expect(model.validationError).toBe(null);
+
+      model.save();
+      
+      expect(model.validationError).toEqual(msg);
+    });
+
+    it('should correctly set values upon save', function() {
+      var summary = 'newSummary';
+      var description = 'newDescription';
+      model.url = '/';
+      
+      model.save({
+        summary: summary,
+        description: description
+      })
+
+      expect(model.get('summary')).toEqual(summary);
+      expect(model.get('description')).toEqual(description);
+    })
   });
 });
