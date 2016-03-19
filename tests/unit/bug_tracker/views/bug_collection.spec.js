e082a0c5479451c3544b7345b9f4a4b395ece4d5
Joel Kraft <joelkraft3@yahoo.com>
2016-03-18 17:18:57 -0700

    test coverage for bug collection view

--- tests/unit/bug_tracker/views/bug_collection.spec.js
+++ tests/unit/bug_tracker/views/bug_collection.spec.js
@@ -10,22 +10,31 @@ define([
       view = new BugCollectionView({
         collection: new Backbone.Collection(),
       });
+      spyOn(view.collection, 'fetch')
     });
 
     it('should exist', function () {
       expect(view).toBeDefined();
     });
 
-    // @TODO write tests to match the implementation :)
     it('should not contain a bug to start', function() {
       var renderedView = view.render();
       var bugs = renderedView.$el.find('.bug_item').length;
       expect(bugs).toBe(0);
     });
+
     it('should be able to add a bug', function() {
       view.addBug({});
       var bugs = view.render().$el.find('.bug_item').length;
       expect(bugs).toBe(1);
+    });
+
+    it('should be able to refresh', function() {
+      view.addBug({});
+      var renderedView = view.render();
+      expect(renderedView.$el.find('.bug_item').length).toBe(1);
+      view.refresh({});
+      expect(renderedView.$el.find('.bug_item').length).toBe(1);
     });
   });
 });
